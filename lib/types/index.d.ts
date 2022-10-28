fdead1c152f555a84482046215dda2cada0b88f4
unknown <abanoubharby@gmail.com>
2022-10-28 18:26:47 +0200

    npm prepare

---
+++ lib/types/index.d.ts
@@ -0,0 +1,41 @@
+import { DiffComponent, DiffComponentOptions, ObjectDiff, ObjectSchemaType, ObjectInputProps, ArrayDiff, DiffProps } from "sanity";
+declare global {
+    interface Window {
+        gm_authFailure: any;
+        ___sanity_googleMapsApiCallback: any;
+    }
+}
+export interface LatLng {
+    lat: number;
+    lng: number;
+}
+export interface Geopoint {
+    _type: 'geopoint';
+    _key?: string;
+    lat: number;
+    lng: number;
+    alt?: number;
+}
+export interface GeopointSchemaType extends ObjectSchemaType {
+    diffComponent?: DiffComponent<ObjectDiff<Geopoint>> | DiffComponentOptions<ObjectDiff<Geopoint>>;
+}
+export type GeopointInputProps = ObjectInputProps<Geopoint, GeopointSchemaType> & {
+    geoConfig: GoogleMapsInputConfig;
+};
+export default GeopointInput;
+export type GeopointArrayDiffProps = DiffProps<ArrayDiff<Geopoint>>;
+export const GeopointArrayDiff: DiffComponent<ArrayDiff<Geopoint>>;
+export type GeopointFieldDiffProps = DiffProps<ObjectDiff<Geopoint>>;
+export const GeopointFieldDiff: DiffComponent<ObjectDiff<Geopoint>>;
+export interface GoogleMapsInputConfig {
+    apiKey: string;
+    defaultZoom?: number;
+    defaultLocale?: string;
+    defaultLocation?: {
+        lat: number;
+        lng: number;
+    };
+}
+export const googleMapsInput: import("sanity").Plugin<GoogleMapsInputConfig>;
+
+//# sourceMappingURL=index.d.ts.map
